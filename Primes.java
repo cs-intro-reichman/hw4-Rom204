99bd800b7c6cfd16a442391e0d24fdfbfa680704
Rom Guyer <romguyer@Roms-MacBook-Pro.local>
2024-11-28 12:47:26 +0200

    added fix

--- Primes.java
+++ Primes.java
@@ -2,13 +2,13 @@ public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]); // nums
         boolean[] primesArr = new boolean[n + 1];
-        
+        int root = (int) Math.sqrt(primesArr.length);
         for (int i = 2; i < primesArr.length; i++){
             primesArr[i] = true;
         }
 
         int c = 2;
-        while (c < primesArr.length){
+        while (c <= root){
             if (primesArr[c] == true) {
                 for (int i = (c + 1); i < primesArr.length; i++){
                     if (i % c == 0 && primesArr[i] == true){
@@ -18,7 +18,7 @@ public class Primes {
             }
             c++;
         }
-
+        System.out.println("Prime numbers up to " + n + ":");
         int primeCount = 0;
         for (int i = 2; i < primesArr.length; i++){
             if (primesArr[i] == true){
@@ -27,7 +27,8 @@ public class Primes {
             }
         }
         double percentage = ((double) primeCount / primesArr.length) * 100;
-        int roundedPercentage = (int) Math.ceil(percentage);
+        int roundedPercentage = (int)Math.ceil(percentage);
+        
         System.out.println("There are " + primeCount + " primes between 2 and " + n + " (" + roundedPercentage + "% are primes)");
     }
 }
